1d849ad6cad7ff841bb6d74a2027f3ed5ec52852
mazax1ct <vova@giveandget.ru>
2018-09-21 14:02:51 +0300

    first

---
+++ build/js/main.js
@@ -0,0 +1,52 @@
+//активация кнопки "наверх"
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('.header__bottom-inner').addClass('scrolled');
+    $('.js-to-top').addClass('is-active');
+  } else {
+    $('.header__bottom-inner').removeClass('scrolled');
+    $('.js-to-top').removeClass('is-active');
+  }
+});
+
+//скролл наверх при нажитии кнопки "наверх"
+$('.js-to-top').click(function () {
+  $('body, html').animate({
+      scrollTop: 0
+  }, 300);
+  return false;
+});
+
+$(document).ready(function() {
+  //слайдер баннер
+  if ($('.js-main-banner').length) {
+    $('.js-main-banner').slick({
+      arrows: true,
+      dots: false,
+      prevArrow: '<button class="slick-prev" type="button" title="Назад"><i class="ion-ios-arrow-back"></i></button>',
+      nextArrow: '<button class="slick-next" type="button" title="Вперед"><i class="ion-ios-arrow-forward"></i></button>'
+    });
+  }
+
+  //открытие/закрытие меню на мобильном
+  $(".js-categories-menu").click(function() {
+    $(this).toggleClass("is-active");
+    $(".categories-menu__list").slideToggle();
+    return false;
+  });
+
+  //открытие/закрытие корзины в шапке
+  $(".js-cart-open").click(function() {
+    $(".small-cart__dropdown").toggleClass('is-open');
+    return false;
+  });
+
+  //закрытие корзины в шапке
+  $(document).click(function(event) {
+    if($('.small-cart__dropdown').hasClass('is-open')) {
+      if($(event.target).closest('.small-cart__dropdown').length) return;
+        $('.small-cart__dropdown').removeClass("is-open");
+        event.stopPropagation();
+    }
+  });
+});
